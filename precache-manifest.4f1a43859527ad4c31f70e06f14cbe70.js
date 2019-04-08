7213de56aea2869084fb9ca2984844e71b01b129
yuting49 <ytcstone@gmail.com>
2019-04-08 15:57:47 +0800

    Updates

---
+++ precache-manifest.4f1a43859527ad4c31f70e06f14cbe70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a54ce66b92b13a0b6678",
+    "url": "/React-todolist/static/css/main.6657dded.chunk.css"
+  },
+  {
+    "revision": "a54ce66b92b13a0b6678",
+    "url": "/React-todolist/static/js/main.e7c7b86f.chunk.js"
+  },
+  {
+    "revision": "8365dc2ea60f80f52a3b",
+    "url": "/React-todolist/static/js/runtime~main.16074bc3.js"
+  },
+  {
+    "revision": "2dc2caf955abf0f4630e",
+    "url": "/React-todolist/static/css/2.45170398.chunk.css"
+  },
+  {
+    "revision": "2dc2caf955abf0f4630e",
+    "url": "/React-todolist/static/js/2.7de7ec91.chunk.js"
+  },
+  {
+    "revision": "8620bd3bd40d4119cc2450cdbd9e798a",
+    "url": "/React-todolist/index.html"
+  }
+];
